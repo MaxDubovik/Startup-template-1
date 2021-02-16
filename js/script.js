2216de246f257636e8a4c20ddaa828d0676f6adf
Max <maks.dubovik.98@gmail.com>
2021-02-17 00:20:21 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $('.header__burger').click(function(event){
         $('.header__burger, .header__menu').toggleClass('active');
     });
+
+    $('.works__tabs').on('click', 'li:not(.works__tab_active)', function() {
+        $(this)
+            .addClass('works__tab_active').siblings().removeClass('works__tab_active')
+            .closest('div.container').find('div.works__content').removeClass('works__content_active').eq($(this).index()).addClass('works__content_active');
+        });
 });
